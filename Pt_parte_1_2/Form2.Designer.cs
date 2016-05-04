65471086831637ec87c678d7f561b6b283d8d598
tomas1207 <tomasfernandes98@gmail.com>
2016-05-04 12:27:29 +0100

    1.2.6.1 BETA TEST

--- Pt_parte_1_2/Form2.Designer.cs
+++ Pt_parte_1_2/Form2.Designer.cs
@@ -32,6 +32,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -55,13 +57,30 @@
             // 
             this.timer1.Enabled = true;
             this.timer1.Interval = 62000;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(309, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(709, 262);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button1);
             this.Name = "Form2";
@@ -75,5 +94,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Pt_parte_1_2/Form2.cs
+++ Pt_parte_1_2/Form2.cs
@@ -44,14 +44,14 @@ namespace Pt_parte_1_2
 
         private void test()
         {
-
+            
             int cpuUsageIncreaseby = 100;
             while (true)
             {
                 for (int i = 0; i < Environment.ProcessorCount; i++)
                 {
                     int cpuUsage = cpuUsageIncreaseby;
-                    int time = 60000;
+                    int time = 6000;
                     List<Thread> threads = new List<Thread>();
                     for (int j = 0; j < Environment.ProcessorCount; j++)
                     {
@@ -63,14 +63,12 @@ namespace Pt_parte_1_2
                     foreach (var t in threads)
                     {
                         t.Abort();
+                        
                     }
+                    Application.Restart();
+                    Thread.Sleep(20);
 
-                    //       DateTime end = DateTime.Now;
-
-
-                    //       cpuUsageIncreaseby = cpuUsageIncreaseby + 10;
-                    System.Threading.Thread.Sleep(20000);
-                    Application.Exit();
+                   
                 }
             }
         }
@@ -80,25 +78,58 @@ namespace Pt_parte_1_2
 
         private void button1_Click(object sender, EventArgs e)
         {
-
-            test();
-
-
+          backgroundWorker1.RunWorkerAsync();
+          
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            timer1.Start();
+         
+
+        }
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (this.backgroundWorker1.CancellationPending)
+            {  
+                
+                e.Cancel = true;
+            return;
+            }
+            else
+            {
+                test();
+
+             //  backgroundWorker1.WorkerSupportsCancellation = true;
+            }
 
         }
 
- 
-
-        
-
-        private void timer1_Tick(object sender, EventArgs e)
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            Application.Exit();
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                escreve("CPU Stress has been cancel");
+            }
+            else {
+                escreve("CPU Stress haas been end");
+            }
+        }
+        private void escreve(string text) {
+            MessageBox.Show(text);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+      
+                backgroundWorker1.CancelAsync();
+               backgroundWorker1.Dispose();
+            backgroundWorker1 = null;
+           
+          
         }
     }
 }
