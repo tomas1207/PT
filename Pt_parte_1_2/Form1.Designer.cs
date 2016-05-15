0bba9f5f84e182e7a1a0c8aaf4abd4bc9a9bd093
tomas1207 <tomasfernandes98@gmail.com>
2016-05-15 18:15:09 +0100

    3.0

---
+++ Pt_parte_1_2/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Pt_parte_1_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ficheirosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.programsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cPUStreesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ficheirosToolStripMenuItem,
+            this.backgroundImageToolStripMenuItem,
+            this.programsToolStripMenuItem,
+            this.cPUStreesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(713, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ficheirosToolStripMenuItem
+            // 
+            this.ficheirosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem,
+            this.removeToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.ficheirosToolStripMenuItem.Name = "ficheirosToolStripMenuItem";
+            this.ficheirosToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.ficheirosToolStripMenuItem.Text = "Ficheiros";
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.addToolStripMenuItem.Text = "Add";
+            this.addToolStripMenuItem.Click += new System.EventHandler(this.addToolStripMenuItem_Click);
+            // 
+            // removeToolStripMenuItem
+            // 
+            this.removeToolStripMenuItem.Name = "removeToolStripMenuItem";
+            this.removeToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.removeToolStripMenuItem.Text = "Remove";
+            this.removeToolStripMenuItem.Click += new System.EventHandler(this.removeToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.editToolStripMenuItem.Text = "Edit";
+            this.editToolStripMenuItem.Click += new System.EventHandler(this.editToolStripMenuItem_Click);
+            // 
+            // backgroundImageToolStripMenuItem
+            // 
+            this.backgroundImageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem1,
+            this.removeToolStripMenuItem1,
+            this.editToolStripMenuItem1});
+            this.backgroundImageToolStripMenuItem.Name = "backgroundImageToolStripMenuItem";
+            this.backgroundImageToolStripMenuItem.Size = new System.Drawing.Size(119, 20);
+            this.backgroundImageToolStripMenuItem.Text = "Background Image";
+            this.backgroundImageToolStripMenuItem.Click += new System.EventHandler(this.backgroundImageToolStripMenuItem_Click);
+            // 
+            // addToolStripMenuItem1
+            // 
+            this.addToolStripMenuItem1.Name = "addToolStripMenuItem1";
+            this.addToolStripMenuItem1.Size = new System.Drawing.Size(117, 22);
+            this.addToolStripMenuItem1.Text = "Add";
+            this.addToolStripMenuItem1.Click += new System.EventHandler(this.addToolStripMenuItem1_Click);
+            // 
+            // removeToolStripMenuItem1
+            // 
+            this.removeToolStripMenuItem1.Name = "removeToolStripMenuItem1";
+            this.removeToolStripMenuItem1.Size = new System.Drawing.Size(117, 22);
+            this.removeToolStripMenuItem1.Text = "Remove";
+            this.removeToolStripMenuItem1.Click += new System.EventHandler(this.removeToolStripMenuItem1_Click);
+            // 
+            // editToolStripMenuItem1
+            // 
+            this.editToolStripMenuItem1.Name = "editToolStripMenuItem1";
+            this.editToolStripMenuItem1.Size = new System.Drawing.Size(117, 22);
+            this.editToolStripMenuItem1.Text = "Edit";
+            this.editToolStripMenuItem1.Click += new System.EventHandler(this.editToolStripMenuItem1_Click);
+            // 
+            // programsToolStripMenuItem
+            // 
+            this.programsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem3,
+            this.removeToolStripMenuItem2});
+            this.programsToolStripMenuItem.Name = "programsToolStripMenuItem";
+            this.programsToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.programsToolStripMenuItem.Text = "Programs";
+            this.programsToolStripMenuItem.Click += new System.EventHandler(this.programsToolStripMenuItem_Click);
+            // 
+            // addToolStripMenuItem3
+            // 
+            this.addToolStripMenuItem3.Name = "addToolStripMenuItem3";
+            this.addToolStripMenuItem3.Size = new System.Drawing.Size(117, 22);
+            this.addToolStripMenuItem3.Text = "Add";
+            this.addToolStripMenuItem3.Click += new System.EventHandler(this.addToolStripMenuItem3_Click);
+            // 
+            // removeToolStripMenuItem2
+            // 
+            this.removeToolStripMenuItem2.Name = "removeToolStripMenuItem2";
+            this.removeToolStripMenuItem2.Size = new System.Drawing.Size(117, 22);
+            this.removeToolStripMenuItem2.Text = "Remove";
+            this.removeToolStripMenuItem2.Click += new System.EventHandler(this.removeToolStripMenuItem2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_2);
+            // 
+            // cPUStreesToolStripMenuItem
+            // 
+            this.cPUStreesToolStripMenuItem.Name = "cPUStreesToolStripMenuItem";
+            this.cPUStreesToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.cPUStreesToolStripMenuItem.Text = "CPUStrees";
+            this.cPUStreesToolStripMenuItem.Click += new System.EventHandler(this.cPUStreesToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 322);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ficheirosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backgroundImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem programsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem cPUStreesToolStripMenuItem;
+    }
+}
+
---
+++ Pt_parte_1_2/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Net;
+
+namespace Pt_parte_1_2
+{
+    public partial class Form1 : Form
+    {
+        int possicaox = 27;
+        int totaldeckcount = 100;
+        int vez = 1;
+        int i= 0;
+        int sub_i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string prog;
+
+           [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hwc, IntPtr hwp);
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Upload files";
+            ofd.Filter = "All type for the programs | *.jpg; *.png";
+           
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                if (pictureBox1.Image == null)
+                {
+                    pictureBox1.Enabled = false;
+
+                      pictureBox1.BackgroundImage = new Bitmap(ofd.FileName);
+                      pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+                   
+              removeToolStripMenuItem.Enabled = true;
+                    addToolStripMenuItem.Enabled = false;
+
+                  }
+                else { MessageBox.Show("Errrrrrrooo!" , "Erro 0" ); }
+            }
+            else { MessageBox.Show("is not a fatal erro", "Erro -1"); }
+            //dlg.Dispose();
+            }
+
+        private void removeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1 != null)
+            {
+                pictureBox1.Enabled = true;
+
+                pictureBox1.BackgroundImage = null;
+                removeToolStripMenuItem.Enabled = false;
+                addToolStripMenuItem.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            removeToolStripMenuItem.Enabled = false;
+            File.Delete("CPUStress.exe");
+
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+              if (pictureBox1.BackgroundImage != null)
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+
+                dlg.Title = "Edit directory";
+                dlg.Filter = "Edit | *.*";
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    
+                    pictureBox1.BackgroundImage = new Bitmap(dlg.FileName);
+
+
+                }
+            }
+            else { MessageBox.Show("Miss image","Erroo 1", MessageBoxButtons.OK);
+            }
+        }
+
+        private void backgroundImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Into image";
+            ofd.Filter = " Image| *.jpg";
+            ofd.FileName = "Image";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                BackgroundImage = new Bitmap(ofd.FileName);
+                BackgroundImageLayout = ImageLayout.Stretch;
+
+
+            }
+        }
+        private void removeToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (BackgroundImage != null) {
+                BackgroundImage = null;
+            }
+        }
+
+        private void editToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Edit image";
+            ofd.FileName = "Upload image";
+            ofd.Filter = "|*.png *.jpg";
+            if (BackgroundImage != null) {
+                if (ofd.ShowDialog() == DialogResult.OK) {
+                    BackgroundImage = null;
+                    BackgroundImage = new Bitmap(ofd.FileName);
+                    BackgroundImageLayout = ImageLayout.Stretch;
+
+                
+                }
+            
+            }
+        }
+
+        private void addToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+            
+            string[] files = Directory.GetFiles(fbd.SelectedPath);
+            MessageBox.Show("Files Found " + files.Length.ToString(), "Message");
+        }
+
+        private void addTry2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+ 
+
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+    
+        }
+
+        private void programsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addToolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+      
+            // add programa
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Upload a program";
+            ofd.Filter = "Program |*.exe*";
+
+            PictureBox[] pbName = new PictureBox[totaldeckcount];
+            Label lb = new Label();
+            for ( i = 0; i < totaldeckcount-99; i++)
+            {
+                lb.Text = i.ToString();
+                pbName[i] = new PictureBox();
+                if (vez == 1)
+                {
+                    pbName[i].Location = new Point(possicaox += 30, 27);
+                }
+                else {
+                    pbName[i].Location = new Point(possicaox += 60, 27);
+                }
+                pbName[i].Size = new Size(32, 32);
+                pbName[i].BackgroundImageLayout = ImageLayout.Stretch;
+                pbName[i].Visible = true;
+                this.Controls.Add(pbName[i]);
+                i = sub_i;
+            }
+            
+            if (vez == 1)
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    Icon icon = Icon.ExtractAssociatedIcon(ofd.FileName);
+                    pictureBox1.Image = icon.ToBitmap();
+                    //end icon
+                    prog = ofd.FileName;
+
+                    vez = 2;
+                }
+
+                else { MessageBox.Show("Not a fatal error np my friend :P", "Errrrrrrrrrrrrooooo but no problem im here because you close me :( "); }
+
+            }
+            else
+            {
+                
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    Icon icon = Icon.ExtractAssociatedIcon(ofd.FileName);
+                    pbName[sub_i].Image = icon.ToBitmap();            
+                    prog = ofd.FileName;
+
+                }
+                else { MessageBox.Show("Erro"); }
+            }
+        }
+
+        
+
+        private void removeToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            // remover Programa
+            prog = null;
+            pictureBox1.Image = null;
+            vez = 1;
+      
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_2(object sender, EventArgs e)
+        {
+            iniciar();
+
+        }
+        private void iniciar()
+        {
+                 //comerçar o programa fora da form
+            if (prog != null)
+            {
+                Process P = Process.Start(prog);
+                Thread.Sleep(2500);
+                P.WaitForInputIdle();
+                SetParent(P.Handle, this.Handle);
+            }
+            else { MessageBox.Show("Erro missing a program", "Erro 2"); }
+        }
+
+        private void cPUStreesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient update_file = new WebClient();
+                Uri update_url = new Uri(Properties.Glogalstring.Cpustress);
+                update_file.DownloadFile(update_url, "CPUStress.exe");
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+            try
+            {
+                var exe = new ProcessStartInfo();
+                exe.FileName = "CPUStress";
+
+                var process = new Process();
+                process.StartInfo = exe;
+                process.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+   }
+}
+    
+
